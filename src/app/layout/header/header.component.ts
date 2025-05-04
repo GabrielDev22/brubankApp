767d693d0452e384a8e28f58364a868aedc8d277
Gabriel <gabrieltorresmora2@gmail.com>
2025-05-04 19:03:11 -0300

    Completando creacion de sub-menu por primera vez visualmente

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { provideHttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { InlineSVGModule } from 'ng-inline-svg-2';
+
+export enum SubMenuItem{
+  PERSONAS = "PERSONAS",
+  COMPANY = "COMPANY"
+}
 
 @Component({
   selector: 'app-header',
@@ -9,8 +13,41 @@ import { InlineSVGModule } from 'ng-inline-svg-2';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   showMegaMenu : boolean = false;
+  showMegaMenuCompany : boolean = false;
+
+  constructor(
+    private cd : ChangeDetectorRef
+  ){}
+
+  ngOnInit() {
+
+  }
+
+  mouseEventSubMenu(action: 'enter' | 'leave', item : string){
+    if(action === 'enter'){
+      if(item == SubMenuItem.PERSONAS){
+        this.showMegaMenu = true;
+        this.showMegaMenuCompany = false;
+      }
+      else if(item == SubMenuItem.COMPANY){
+        this.showMegaMenuCompany = true;
+        this.showMegaMenu = false;
+      }
+    } else{
+      if (item === SubMenuItem.PERSONAS) {
+        this.showMegaMenu = false;
+      } else if (item === SubMenuItem.COMPANY) {
+        this.showMegaMenuCompany = false;
+      }
+    }
+    this.cd.markForCheck();
+  }
+
+  get SubMenuItem(){
+    return SubMenuItem;
+  }
 
 }
